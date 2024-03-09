906222e3610b09999fb7fa69e76d649c592bc590
Enrique De Jesús Espinoza Salas <terryrockstar22@gmail.com>
2024-03-09 12:24:44 -0600

    chore: Update test port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { v4: uuidv4 } = require('uuid');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3001;
+const PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(cors())
